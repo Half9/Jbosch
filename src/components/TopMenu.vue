64dbe385fc658b7f70b051c77b4194bad9764311
Half9 <3681520+Half9@users.noreply.github.com>
2022-09-22 13:59:36 +0200

    First commit

---
+++ src/components/TopMenu.vue
@@ -0,0 +1,8 @@
+<template>
+    <ul>
+        <li><a href="">Home</a></li>
+        <li><a href="#overmij">Over mij</a></li>
+        <li><a href="#projecten">Projecten</a></li>
+        <li><a href="#contact">Contact</a></li>
+    </ul>
+</template>
